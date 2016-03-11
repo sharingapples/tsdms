def21f66ad1582f8f91d6ec1ae894ceada9ab8ad
Ranjan Shrestha <ranjan@sharingapples.com>
2016-03-11 12:18:52 +0545

    Added Database migrations

--- db/migrations/20160308143130_system.js
+++ db/migrations/20160308143130_system.js
@@ -2,9 +2,9 @@
 var User = require('../../api/models/User');
 
 exports.up = function(knex, Promise) {
-
-    // Create the user table
-    return knex.schema.createTable('user', function(table) {
+  // First create all the independent tables
+  return Promise.all([
+    knex.schema.createTable('user', function(table) {
       table.increments().primary();
       table.string('username').unique().notNullable();
       table.string('password').notNullable();
@@ -14,7 +14,7 @@ exports.up = function(knex, Promise) {
       table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
       table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
     }).then(function() {
-      // Insert the super admin user
+      // Add a super admin user with default configuration
       return new Promise(function(resolve, reject) {
         User.beforeCreate({
           username: 'admin',
@@ -29,26 +29,32 @@ exports.up = function(knex, Promise) {
           resolve(knex('user').insert(user));
         });
       })
-    }).then(function() {
-      return knex.schema.createTable('group', function(table) {
-        table.increments().primary();
-        table.string('name').unique().notNullable();
-        table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
-        table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
-      });
-    }).then(function() {
-      return knex.schema.createTable('user_group', function(table) {
+    }),
+    knex.schema.createTable('group', function(table) {
+      table.increments().primary();
+      table.string('name').unique().notNullable();
+      table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
+      table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
+    })
+  ]).then(function() {
+    // Create the dependent tables
+    return Promise.all([
+      knex.schema.createTable('user_group', function(table) {
         table.integer('user_id').notNullable().references('id').inTable('user');
         table.integer('group_id').notNullable().references('id').inTable('group');
         table.primary(['user_id', 'group_id']);
-      });
-    });
+      })
+    ])
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('user_group').then(function() {
-    return knex.schema.dropTable('user');
-  }).then(function() {
-    return knex.schema.dropTable('group');
+  // First drop all the dependent tables
+  return Promise.all([
+    knex.schema.dropTable('user_group')
+  ]).then(function() {
+    // Drop all the independent tables
+    knex.schema.dropTable('group'),
+    knex.schema.dropTable('user')
   });
 };
--- db/migrations/20160308143138_core.js
+++ db/migrations/20160308143138_core.js
@@ -1,20 +1,22 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  // Create the station table
-  return knex.schema.createTable('station', function(table) {
-      table.increments().primary();
-      table.string('identifier').unique().notNullable();
-      table.string('name').notNullable();
-      table.float('longitude').nullable();
-      table.float('latitude').nullable();
-      table.float('elevation').nullable();
-      table.string('description').nullable();
-      table.date('established').nullable();
-      table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
-      table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
-  }).then(function() {
-    return knex.schema.createTable('tag', function(table) {
+  // First off create all the core tables
+  return Promise.all([
+    knex.schema.createTable('station', function(table) {
+        table.increments().primary();
+        table.string('identifier').unique().notNullable();
+        table.string('name').notNullable();
+        table.float('longitude').nullable();
+        table.float('latitude').nullable();
+        table.float('elevation').nullable();
+        table.string('description').nullable();
+        table.date('established').nullable();
+        table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
+        table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
+    }),
+
+    knex.schema.createTable('tag', function(table) {
       table.increments().primary();
       table.string('name').unique().notNullable();
       table.string('description');
@@ -22,8 +24,29 @@ exports.up = function(knex, Promise) {
       table.boolean('starred');     // Seems unused at the moment
       table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
       table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
-    }).then(function() {
-      return knex.schema.createTable('tag_station', function(table) {
+    }),
+
+    knex.schema.createTable('meta_data', function(table) {
+      table.increments().primary();
+      table.string('name').unique().notNullable();
+      table.string('caption').notNullable();
+      table.string('description');
+      table.integer('regex');
+      table.integer('seq');
+      table.boolean('flagged');
+    }),
+
+    knex.schema.createTable('file', function(table) {
+      table.increments().primary();
+      table.string('name').unique().notNullable();
+      table.integer('size').notNullable();
+      table.integer('type').notNullable();
+      table.string('description');
+    })
+  ]).then(function() {
+    // Then create all the dependent tables
+    return Promise.all([
+      knex.schema.createTable('tag_station', function(table) {
         table.increments().primary();
         table.integer('tag_id').notNullable().references('tag.id');
         table.integer('station_id').notNullable().references('station.id');
@@ -31,15 +54,46 @@ exports.up = function(knex, Promise) {
         table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
         table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
         table.unique(['tag_id', 'unique_id']);
-      });
-    });
+      }),
+
+      knex.schema.createTable('meta_data_option', function(table) {
+        table.increments().primary();
+        table.integer('meta_data_id').notNullable().references('meta_data.id');
+        table.string('value').notNullable();
+        table.string('text').notNullable();
+        table.integer('seq');
+      }),
+      knex.schema.createTable('station_meta_data', function(table) {
+        table.increments().primary();
+        table.integer('station_id').notNullable().references('station.id');
+        table.integer('meta_data_id').notNullable().references('meta_data.id');
+        table.string('value').notNullable();
+        table.unique(['station_id', 'meta_data_id'])
+      }),
+
+      knex.schema.createTable('station_file', function(table) {
+        table.integer('station_id').notNullable().references('station.id');
+        table.integer('file_id').notNullable().references('file.id');
+        table.primary(['station_id', 'file_id']);
+      })
+    ]);
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('tag_station').then(function() {
-    return knex.schema.dropTable('tag')
-  }).then(function() {
-    return knex.schema.dropTable('tag_station');
+  // First drop all the table for which there are no dependencies
+  return Promise.all([
+    knex.schema.dropTable('tag_station'),
+    knex.schema.dropTable('station_file'),
+    knex.schema.dropTable('meta_data_option'),
+    knex.schema.dropTable('station_meta_data'),
+  ]).then(function() {
+    // Finally all the core tables
+    return Promise.all([
+      knex.schema.dropTable('file'),
+      knex.schema.dropTable('meta_data'),
+      knex.schema.dropTable('tag'),
+      knex.schema.dropTable('station')
+    ]);
   });
 };
