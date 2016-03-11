04652770f2ae9a6b723e31f3debeacf054b8da3c
Ranjan Shrestha <ranjan@sharingapples.com>
2016-03-12 01:07:39 +0545

    Added error handling in automatic model generation script

--- scripts/ui-model-generator.js
+++ scripts/ui-model-generator.js
@@ -1,5 +1,5 @@
 "use strict";
-
+var md5 = require('md5');
 var fs = require('fs');
 var path = require('path');
 
@@ -17,15 +17,42 @@ fs.readdir(modelPath, function(err, files) {
   files.forEach(function(file) {
     // Only process the js files
     if (file.endsWith('.js')) {
-      var model = require(modelPath + '/' + file);
+      var name = file.substr(0, file.length-3);
+      var srcFile = modelPath + '/' + file;
+      var targetFile = targetDir + '/' + file;
+
+      var model = require(srcFile);
+      var hash = md5(fs.readFileSync(modelPath + '/' + file));
+      var fileModified = false;
+      // See if the targetFile exists
+      var signature;
+      if (fs.existsSync(targetFile)) {
+        // If nothing has changed, no need to generate a target file
+        var targetModel = require(targetFile);
+        if (targetModel.__hash__ === hash) {
+          // The file has not changed, no need to generate target output
+          console.log("✓ " + file + " :: No changes");
+          return;
+        }
+
+        // Let's see if the user has manually changed the target file
+        var data = fs.readFileSync(targetFile, "utf-8");
+        var matches = data.match(/@signature: ([0-9abcdef]{32})/i);
+        if (matches) {
+          signature = matches[1];
+        }
+        var offset = data.search(/module\.exports = \{/);
+        if (offset == -1 || md5(data.substring(offset, data.length)) != signature) {
+          console.error("✗ " + file + " :: Manually modified. Please update this file manually.");
+          return;
+        }
+      }
+
       var attributes = model.attributes;
-      var content =`/**
- * Automatically generated model file
- * @time: ${now.toLocaleString()}
- * @source: ${"api/models/" + file}
- */
- module.exports = {
-  attributes: [
+      var content = `module.exports = {
+   __name__: ${JSON.stringify(name)},
+   __hash__: ${JSON.stringify(hash)},
+   attributes: [
     `;
       for(var attr in attributes) {
         // Only take attributes that are object and have a property named 'type'
@@ -35,12 +62,23 @@ fs.readdir(modelPath, function(err, files) {
           content += JSON.stringify(attributes[attr]) + ",\n    ";
         }
       }
-      content += "]\n}";
+      content += "]\n}\n";
 
-      fs.writeFile(targetDir + '/' + file, content, function(err) {
+      // Generate the signature for the content
+      signature = md5(content);
+      var header =`/**
+ * Automatically generated model file
+ * @time: ${now.toLocaleString()}
+ * @source: ${"api/models/" + file}
+ * @signature: ${signature}
+ */`;
+
+      fs.writeFile(targetFile, header + "\n" + content, function(err) {
         if (err) {
-          console.error("Error while generating model - ", file);
+          console.error("✗ " + file + " :: File System Error. Could not write.");
           console.error(err);
+        } else {
+          console.log("✓ " + file + " :: New file generated");
         }
       });
 
