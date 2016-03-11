f6440a98a368a5d67521cadd089d84a68bb01bbf
Ranjan Shrestha <ranjan@sharingapples.com>
2016-03-11 23:00:37 +0545

    Added UI specific properties

--- api/models/Station.js
+++ api/models/Station.js
@@ -3,26 +3,51 @@ module.exports = {
     identifier: {
       type: 'string',
       required: true,
-      unique: true
+      unique: true,
+
+      __caption__: "ID",
+      __description__: "A unique identifier that identifies this station"
     },
+
     name: {
       type: 'string',
-      required: true
+      required: true,
+
+      __caption__: "Name",
+      __description__: "A human readable name for the station"
     },
+
     longitude: {
-      type: 'float'
+      type: 'float',
+
+      __caption__: "Longitude",
+      __suffix__: "°E"
     },
+
     latitude: {
-      type: 'float'
+      type: 'float',
+
+      __caption__: "Latitude",
+      __suffix__: "°N"
     },
+
     elevation: {
-      type: 'float'
+      type: 'float',
+
+      __caption__: "Elevation",
+      __suffix__: "m"
     },
+
     description: {
-      type: 'string'
+      type: 'string',
+
+      __caption__: "Description"
     },
+
     established: {
-      type: 'date'
+      type: 'date',
+
+      __caption__: "Established"
     }
   }
 }
