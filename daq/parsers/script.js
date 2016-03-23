a2307e85160bf68e1aaecd3fcb554b1c58be4f78
Ranjan Shrestha <ranjan@sharingapples.com>
2016-03-23 16:37:49 +0545

    Started working on filters

---
+++ daq/parsers/script.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parameters: {
+    name: "code",
+    label: "Javascript code",
+    defaultValue: `
+function(dataSource, data, buffer) {
+  //
+}
+`
+  }
+}
---
+++ daq/protocols/http.js
@@ -0,0 +1,17 @@
+module.exports = {
+  filters = [{
+    name: "path",
+    label: "Path",
+    type: "text"
+  }, {
+    name: "query",
+    label: "Query",
+    type: "map"
+  }, {
+    name: "header",
+    label: "Header",
+    type: "map"
+  }],
+
+
+}
