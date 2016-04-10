1e2c4ddc22ed3e3143211c773345b54be8c7320c
Ranjan Shrestha <ranjan@sharingapples.com>
2016-04-10 12:22:50 +0545

    Fixed the problem with the path with parameters

--- react/components/layout/Application.js
+++ react/components/layout/Application.js
@@ -6,28 +6,38 @@ import TopBar from './TopBar';
 import ContentNavigation from '../ContentNavigation';
 
 class Application extends React.Component {
+  _fixPath(path) {
+    if (!path) {
+      return path;
+    }
+
+    return path.replace(/:([a-z_A-Z0-9]*)/g, (match, p1) => this.props.params[p1]);
+  }
+
   render() {
     // The root path for application (should be '/app/')
-    const rootPath = this.props.route.path;
+    const rootPath = this._fixPath(this.props.route.path);
 
     // Also detect the selected route
     const topLevelRoute = this.props.routes[1];
-    const selected = topLevelRoute.indexRoute ? topLevelRoute.indexRoute.component : topLevelRoute.component;
+    const selected = topLevelRoute.indexRoute ? topLevelRoute.indexRoute.component
+          : topLevelRoute.component;
 
     const { initialData, isServer, user } = this.context;
     const navigations = this.props.route.childRoutes
-              .map( route => (route.childRoutes ? route.indexRoute.component : route.component) )
-              .filter( component => !!component.title)
-              .map( component => ({ path: (rootPath + component.path), title: component.title, selected: component===selected }))
+              .map(route => (route.childRoutes ? route.indexRoute.component : route.component))
+              .filter(component => !!component.title)
+              .map(component => ({
+                  path: (rootPath + component.path),
+                  title: component.title,
+                  selected: component === selected,
+                }));
 
     // if the index route is allowed we will put it as well
     if (this.props.route.indexRoute.component.title) {
       const r = this.props.route.indexRoute.component;
-      navigations.unshift({ path: rootPath, title: r.title, selected: (r===selected)});
+      navigations.unshift({ path: rootPath, title: r.title, selected: (r === selected), });
     }
-
-
-
 
     return (
       <SailsApp initialData={initialData} isServer={isServer}>
@@ -36,7 +46,10 @@ class Application extends React.Component {
           <div className="content">
             <div className="top">
               <TopBar user={user}/>
-              <ContentNavigation pathPrefix={rootPath + topLevelRoute.path} root={selected} selected={this.props.routes[this.props.routes.length-1].component}/>
+              <ContentNavigation
+                  pathPrefix={rootPath + this._fixPath(topLevelRoute.path)}
+                  root={selected}
+                  selected={this.props.routes[this.props.routes.length - 1].component}/>
             </div>
             <div className="content-body">
               <div className="ui segment">
@@ -50,12 +63,11 @@ class Application extends React.Component {
   }
 }
 
-
 // The context is provided by RouterApp (./routes.js)
 Application.contextTypes = {
   user: React.PropTypes.object.isRequired,
   isServer: React.PropTypes.bool.isRequired,
-  initialData: React.PropTypes.object.isRequired
-}
+  initialData: React.PropTypes.object.isRequired,
+};
 
 export default Application;
