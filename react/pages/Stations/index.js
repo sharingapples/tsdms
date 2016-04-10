f04fa54e05da941d591771bf4e4d6719b207e3d1
Ranjan Shrestha <ranjan@sharingapples.com>
2016-04-10 11:14:21 +0545

    Standardized Javascript

--- react/pages/Stations/index.js
+++ react/pages/Stations/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 class Stations extends React.Component {
   render() {
-    return <div>List of Stations</div>
+    return (
+      <div>List of Stations</div>
+    );
   }
 }
 
-Stations.path = "stations/";
-Stations.title = "Stations";    // Displayed on the navigation dock
-Stations.indexTitle = "All";    // Displayed on the content navigation
+Stations.path = 'stations/';
+Stations.title = 'Stations';    // Displayed on the navigation dock
+Stations.indexTitle = 'All';    // Displayed on the content navigation
 
-Stations.options = { "New Station": function() {
-  window.alert("New Station");
-} };
+Stations.options = {
+  'New Station': function () {
+    window.alert('New Station');
+  },
+};
 
 export default Stations;
